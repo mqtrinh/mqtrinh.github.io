74d704a16b960afe2a16e8a6fe973b7e5b5fe750
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-03-12 15:41:11 -0400

    Add files via upload

---
+++ math/research/code/chevie/solve.sh
@@ -0,0 +1,24 @@
+python3 py/dedup_vectors.py exotic/exotic_b2.txt exotic/exotic_b2_trimmed.txt
+python3 py/find_combinations_lp.py exotic/exotic_b2_trimmed.txt
+python3 py/permute_vectors.py exotic/exotic_b2_trimmed.txt exotic/exotic_b2_sorted.txt 2 3 5 1 4
+python3 py/mv_multiply.py exotic/exotic_b2_sorted.txt icc/icc_inverted_b2.txt exotic/exotic_b2_solved.txt
+
+python3 py/dedup_vectors.py exotic/exotic_b3.txt exotic/exotic_b3_trimmed.txt
+python3 py/find_combinations_lp.py exotic/exotic_b3_trimmed.txt
+python3 py/permute_vectors.py exotic/exotic_b3_trimmed.txt exotic/exotic_b3_sorted.txt 7 6 8 10 2 5 3 9 1 4
+python3 py/mv_multiply.py exotic/exotic_b3_sorted.txt icc/icc_inverted_b3.txt exotic/exotic_b3_solved.txt
+
+python3 py/dedup_vectors.py exotic/exotic_b4.txt exotic/exotic_b4_trimmed.txt
+python3 py/find_combinations_lp.py exotic/exotic_b4_trimmed.txt
+python3 py/permute_vectors.py exotic/exotic_b4_trimmed.txt exotic/exotic_b4_sorted.txt 17 14 15 18 20 9 11 8 13 10 19 5 6 16 2 7 3 12 1 4
+python3 py/mv_multiply.py exotic/exotic_b4_sorted.txt icc/icc_inverted_b4.txt exotic/exotic_b4_solved.txt
+
+python3 py/dedup_vectors.py exotic/exotic_d4.txt exotic/exotic_d4_trimmed.txt
+python3 py/find_combinations_lp.py exotic/exotic_d4_trimmed.txt
+python3 py/permute_vectors.py exotic/exotic_d4_trimmed.txt exotic/exotic_d4_sorted.txt 11 10 12 13 6 7 9 5 4 8 2 3 1
+python3 py/mv_multiply.py exotic/exotic_d4_sorted.txt icc/icc_inverted_d4.txt exotic/exotic_d4_solved.txt
+
+python3 py/dedup_vectors.py exotic/exotic_g2.txt exotic/exotic_g2_trimmed.txt
+python3 py/find_combinations_lp.py exotic/exotic_g2_trimmed.txt
+python3 py/permute_vectors.py exotic/exotic_g2_trimmed.txt exotic/exotic_g2_sorted.txt 1 6 2 5 3 4
+python3 py/mv_multiply.py exotic/exotic_g2_sorted.txt icc/icc_inverted_g2.txt exotic/exotic_g2_solved.txt
