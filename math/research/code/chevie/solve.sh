d2e6d41a4e5c381c14c5eba05c6fd6b8b71b7160
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-03-13 03:28:40 -0400

    Add files via upload

--- math/research/code/chevie/solve.sh
+++ math/research/code/chevie/solve.sh
@@ -1,24 +1,53 @@
-python3 py/dedup_vectors.py exotic/exotic_b2.txt exotic/exotic_b2_trimmed.txt
-python3 py/find_combinations_lp.py exotic/exotic_b2_trimmed.txt
-python3 py/permute_vectors.py exotic/exotic_b2_trimmed.txt exotic/exotic_b2_sorted.txt 2 3 5 1 4
-python3 py/mv_multiply.py exotic/exotic_b2_sorted.txt icc/icc_inverted_b2.txt exotic/exotic_b2_solved.txt
+# b2
 
-python3 py/dedup_vectors.py exotic/exotic_g2.txt exotic/exotic_g2_trimmed.txt
-python3 py/find_combinations_lp.py exotic/exotic_g2_trimmed.txt
-python3 py/permute_vectors.py exotic/exotic_g2_trimmed.txt exotic/exotic_g2_sorted.txt 1 6 2 5 3 4
-python3 py/mv_multiply.py exotic/exotic_g2_sorted.txt icc/icc_inverted_g2.txt exotic/exotic_g2_solved.txt
+#python3 py/dedup_vectors.py exotic/exotic_b2.txt exotic/exotic_b2_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_b2_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_b2_trimmed.txt exotic/exotic_b2_sorted.txt 2 3 5 1 4
+#python3 py/mv_multiply.py exotic/exotic_b2_sorted.txt icc/icc_inverted_b2.txt exotic/exotic_b2_solved.txt
 
-python3 py/dedup_vectors.py exotic/exotic_b3.txt exotic/exotic_b3_trimmed.txt
-python3 py/find_combinations_lp.py exotic/exotic_b3_trimmed.txt
-python3 py/permute_vectors.py exotic/exotic_b3_trimmed.txt exotic/exotic_b3_sorted.txt 7 6 8 10 2 5 3 9 1 4
-python3 py/mv_multiply.py exotic/exotic_b3_sorted.txt icc/icc_inverted_b3.txt exotic/exotic_b3_solved.txt
+python3 py/find_negatives.py exotic/exotic_b2_solved.txt exotic/exotic_b2_negatives.txt 
 
-python3 py/dedup_vectors.py exotic/exotic_b4.txt exotic/exotic_b4_trimmed.txt
-python3 py/find_combinations_lp.py exotic/exotic_b4_trimmed.txt
-python3 py/permute_vectors.py exotic/exotic_b4_trimmed.txt exotic/exotic_b4_sorted.txt 17 14 15 18 20 9 11 8 13 10 19 5 6 16 2 7 3 12 1 4
-python3 py/mv_multiply.py exotic/exotic_b4_sorted.txt icc/icc_inverted_b4.txt exotic/exotic_b4_solved.txt
+#g2
 
-python3 py/dedup_vectors.py exotic/exotic_d4.txt exotic/exotic_d4_trimmed.txt
-python3 py/find_combinations_lp.py exotic/exotic_d4_trimmed.txt
-python3 py/permute_vectors.py exotic/exotic_d4_trimmed.txt exotic/exotic_d4_sorted.txt 11 10 12 13 6 7 9 5 4 8 2 3 1
-python3 py/mv_multiply.py exotic/exotic_d4_sorted.txt icc/icc_inverted_d4.txt exotic/exotic_d4_solved.txt
+#python3 py/dedup_vectors.py exotic/exotic_g2.txt exotic/exotic_g2_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_g2_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_g2_trimmed.txt exotic/exotic_g2_sorted.txt 1 6 2 5 3 4
+#python3 py/mv_multiply.py exotic/exotic_g2_sorted.txt icc/icc_inverted_g2.txt exotic/exotic_g2_solved.txt
+
+python3 py/find_negatives.py exotic/exotic_g2_solved.txt exotic/exotic_g2_negatives.txt 
+
+# b3
+
+#python3 py/dedup_vectors.py exotic/exotic_b3.txt exotic/exotic_b3_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_b3_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_b3_trimmed.txt exotic/exotic_b3_sorted.txt 7 6 8 10 2 5 3 9 1 4
+#python3 py/mv_multiply.py exotic/exotic_b3_sorted.txt icc/icc_inverted_b3.txt exotic/exotic_b3_solved.txt
+
+python3 py/find_negatives.py exotic/exotic_b3_solved.txt exotic/exotic_b3_negatives.txt 
+
+# b4
+
+#python3 py/dedup_vectors.py exotic/exotic_b4.txt exotic/exotic_b4_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_b4_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_b4_trimmed.txt exotic/exotic_b4_sorted.txt 17 14 15 18 20 9 11 8 13 10 19 5 6 16 2 7 3 12 1 4
+#python3 py/mv_multiply.py exotic/exotic_b4_sorted.txt icc/icc_inverted_b4.txt exotic/exotic_b4_solved.txt
+
+python3 py/find_negatives.py exotic/exotic_b4_solved.txt exotic/exotic_b4_negatives.txt 
+
+# d4
+
+#python3 py/dedup_vectors.py exotic/exotic_d4.txt exotic/exotic_d4_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_d4_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_d4_trimmed.txt exotic/exotic_d4_sorted.txt 11 10 12 13 6 7 9 5 4 8 2 3 1
+#python3 py/mv_multiply.py exotic/exotic_d4_sorted.txt icc/icc_inverted_d4.txt exotic/exotic_d4_solved.txt
+
+python3 py/find_negatives.py exotic/exotic_d4_solved.txt exotic/exotic_d4_negatives.txt 
+
+# f4
+
+#python3 py/dedup_vectors.py exotic/exotic_f4.txt exotic/exotic_f4_trimmed.txt
+#python3 py/find_combinations_lp.py exotic/exotic_f4_trimmed.txt
+#python3 py/permute_vectors.py exotic/exotic_f4_trimmed.txt exotic/exotic_f4_sorted.txt 1 19 8 25 4 22 2 24 15 5 16 10 21 14 9 11 3 17 12 23 7 18 6 20 13
+#python3 py/mv_multiply.py exotic/exotic_f4_sorted.txt icc/icc_inverted_f4.txt exotic/exotic_f4_solved.txt
+
+#python3 py/find_negatives.py exotic/exotic_f4_solved.txt exotic/exotic_f4_negatives.txt 
