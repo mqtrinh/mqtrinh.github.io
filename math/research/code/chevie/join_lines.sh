f151b39801b31d4ce5b09113eb39ebaeb6142fd6
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-03-16 05:07:06 -0400

    Add files via upload

---
+++ math/research/code/chevie/join_lines.sh
@@ -0,0 +1,24 @@
+# all
+
+python3 py/join_lines.py test/all/b3_all.txt test/all/b3_all.txt
+
+python3 py/join_lines.py test/all/b4_all.txt test/all/b4_all.txt
+python3 py/join_lines.py test/all/d4_all.txt test/all/d4_all.txt
+python3 py/join_lines.py test/all/f4_all.txt test/all/f4_all.txt
+
+python3 py/join_lines.py test/all/d5_all.txt test/all/d5_all.txt
+
+# min
+
+python3 py/join_lines.py test/min/b4_min.txt test/min/b4_min.txt
+python3 py/join_lines.py test/min/d4_min.txt test/min/d4_min.txt
+python3 py/join_lines.py test/min/f4_min.txt test/min/f4_min.txt
+
+python3 py/join_lines.py test/min/b5_min.txt test/min/b5_min.txt
+python3 py/join_lines.py test/min/d5_min.txt test/min/d5_min.txt
+python3 py/join_lines.py test/min/d6_min.txt test/min/d6_min.txt
+
+python3 py/join_lines.py test/min/test.txt test/min/test.txt
+
+python3 py/join_lines.py test/min/e6_min.txt test/min/e6_min.txt
+
--- math/research/code/chevie/solve.sh
+++ math/research/code/chevie/solve.sh
@@ -4,10 +4,11 @@
 #python3 py/find_permutations.py
 
 python3 py/sort_vectors.py
-python3 py/trim_vectors.py
+#python3 py/trim_vectors.py # only in mode 'all'
 python3 py/solve_vectors.py
 
-python3 py/sum_families.py
+python3 py/test_solved.py
 
-python3 py/test_conjecture.py
+#python3 py/sum_families.py
+#python3 py/test_summed.py
 
