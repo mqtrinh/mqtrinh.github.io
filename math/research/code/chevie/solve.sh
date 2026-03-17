213787e2d215ae9ff508ed6e5890c74e062f3903
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-03-17 02:18:30 -0400

    Add files via upload

--- math/research/code/chevie/solve.sh
+++ math/research/code/chevie/solve.sh
@@ -3,13 +3,19 @@
 #python3 py/extract_icc_labels.py
 #python3 py/find_permutations.py
 
-#python3 py/permute_vectors.py min
-#python3 py/solve_vectors.py min
-#python3 py/test_solved.py min
+python3 py/permute_vectors.py min
+python3 py/solve_vectors.py min
+python3 py/test_solved.py min
+
+python3 py/sum_families.py min
+python3 py/test_summed.py min
 
 python3 py/permute_vectors.py all
 python3 py/solve_vectors.py all
 python3 py/test_solved.py all
+
+python3 py/sum_families.py all
+python3 py/test_summed.py all
 
 #python3 py/find_duplicates.py # only does mode 'all'
 
