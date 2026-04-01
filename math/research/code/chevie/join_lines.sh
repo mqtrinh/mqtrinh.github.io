db8136d3c23af90b3a08d6aad5e0cb68e8e609be
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-04-01 00:43:49 -0400

    Add files via upload

--- math/research/code/chevie/join_lines.sh
+++ math/research/code/chevie/join_lines.sh
@@ -8,21 +8,58 @@ python3 py/join_lines.py test/all/c4_all_raw.txt test/all/c4_all_raw.txt
 python3 py/join_lines.py test/all/d4_all_raw.txt test/all/d4_all_raw.txt
 python3 py/join_lines.py test/all/f4_all_raw.txt test/all/f4_all_raw.txt
 
-python3 py/join_lines.py test/all/b5_all_raw_incomplete.txt test/all/b5_all_raw.txt
+python3 py/join_lines.py test/all/b5_all_raw.txt test/all/b5_all_raw.txt
+python3 py/join_lines.py test/all/c5_all_raw.txt test/all/c5_all_raw.txt
 python3 py/join_lines.py test/all/d5_all_raw.txt test/all/d5_all_raw.txt
+
+# defectless
+
+#python3 py/join_lines.py test/defectless/b3_defectless_raw.txt test/defectless/b3_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/c3_defectless_raw.txt test/defectless/c3_defectless_raw.txt
+
+#python3 py/join_lines.py test/defectless/b4_defectless_raw.txt test/defectless/b4_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/c4_defectless_raw.txt test/defectless/c4_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/d4_defectless_raw.txt test/defectless/d4_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/f4_defectless_raw.txt test/defectless/f4_defectless_raw.txt
+
+#python3 py/join_lines.py test/defectless/b5_defectless_raw.txt test/defectless/b5_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/c5_defectless_raw.txt test/defectless/c5_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/d5_defectless_raw.txt test/defectless/d5_defectless_raw.txt
+
+#python3 py/join_lines.py test/defectless/b6_defectless_raw.txt test/defectless/b6_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/d6_defectless_raw_incomplete.txt test/defectless/d6_defectless_raw.txt
+#python3 py/join_lines.py test/defectless/e6_defectless_raw.txt test/defectless/e6_defectless_raw.txt
+
+# ratlsmooth
+
+python3 py/join_lines.py test/ratlsmooth/b3_ratlsmooth_raw.txt test/ratlsmooth/b3_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/c3_ratlsmooth_raw.txt test/ratlsmooth/c3_ratlsmooth_raw.txt
+
+python3 py/join_lines.py test/ratlsmooth/b4_ratlsmooth_raw.txt test/ratlsmooth/b4_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/c4_ratlsmooth_raw.txt test/ratlsmooth/c4_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/d4_ratlsmooth_raw.txt test/ratlsmooth/d4_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/f4_ratlsmooth_raw.txt test/ratlsmooth/f4_ratlsmooth_raw.txt
+
+python3 py/join_lines.py test/ratlsmooth/b5_ratlsmooth_raw.txt test/ratlsmooth/b5_ratlsmooth_raw.txt
+#python3 py/join_lines.py test/ratlsmooth/c5_ratlsmooth_raw.txt test/ratlsmooth/c5_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/d5_ratlsmooth_raw.txt test/ratlsmooth/d5_ratlsmooth_raw.txt
+
+python3 py/join_lines.py test/ratlsmooth/b6_ratlsmooth_raw_incomplete.txt test/ratlsmooth/b6_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/d6_ratlsmooth_raw.txt test/ratlsmooth/d6_ratlsmooth_raw.txt
+python3 py/join_lines.py test/ratlsmooth/e6_ratlsmooth_raw_incomplete.txt test/ratlsmooth/e6_ratlsmooth_raw.txt
 
 # min
 
-python3 py/join_lines.py test/min/b4_min_raw.txt test/min/b4_min_raw.txt
-python3 py/join_lines.py test/min/c4_min_raw.txt test/min/c4_min_raw.txt
-python3 py/join_lines.py test/min/d4_min_raw.txt test/min/d4_min_raw.txt
-python3 py/join_lines.py test/min/f4_min_raw.txt test/min/f4_min_raw.txt
+#python3 py/join_lines.py test/min/b4_min_raw.txt test/min/b4_min_raw.txt
+#python3 py/join_lines.py test/min/c4_min_raw.txt test/min/c4_min_raw.txt
+#python3 py/join_lines.py test/min/d4_min_raw.txt test/min/d4_min_raw.txt
+#python3 py/join_lines.py test/min/f4_min_raw.txt test/min/f4_min_raw.txt
 
-python3 py/join_lines.py test/min/b5_min_raw.txt test/min/b5_min_raw.txt
-python3 py/join_lines.py test/min/c5_min_raw.txt test/min/c5_min_raw.txt
-python3 py/join_lines.py test/min/d5_min_raw.txt test/min/d5_min_raw.txt
+#python3 py/join_lines.py test/min/b5_min_raw.txt test/min/b5_min_raw.txt
+#python3 py/join_lines.py test/min/c5_min_raw.txt test/min/c5_min_raw.txt
+#python3 py/join_lines.py test/min/d5_min_raw.txt test/min/d5_min_raw.txt
 
-python3 py/join_lines.py test/min/b6_min_raw.txt test/min/b6_min_raw.txt
-python3 py/join_lines.py test/min/d6_min_raw.txt test/min/d6_min_raw.txt
-python3 py/join_lines.py test/min/e6_min_raw.txt test/min/e6_min_raw.txt
+#python3 py/join_lines.py test/min/b6_min_raw.txt test/min/b6_min_raw.txt
+#python3 py/join_lines.py test/min/d6_min_raw.txt test/min/d6_min_raw.txt
+#python3 py/join_lines.py test/min/e6_min_raw.txt test/min/e6_min_raw.txt
 
--- math/research/code/chevie/solve.sh
+++ math/research/code/chevie/solve.sh
@@ -1,19 +1,29 @@
-python3 py/extract_icc_labels.py
-python3 py/find_permutations.py
-python3 py/csv_families.py
-
-python3 py/csv_icc.py
-python3 py/csv_invert.py icc
-#python3 py/csv_permute.py exotic
-#python3 py/csv_invert.py exotic
-
-python3 py/permute_vectors.py min
-python3 py/csv_multiply.py icc min
-python3 py/test_solved.py min
+#python3 py/extract_icc_labels.py
+#python3 py/find_permutations.py
+#python3 py/csv_families.py
+#python3 py/csv_icc.py
+#python3 py/csv_invert.py icc
 
 python3 py/permute_vectors.py all
 python3 py/csv_multiply.py icc all
 python3 py/test_solved.py all
+
+#python3 py/permute_vectors.py defectless
+#python3 py/csv_multiply.py icc defectless
+#python3 py/test_solved.py defectless
+
+python3 py/permute_vectors.py ratlsmooth
+python3 py/csv_multiply.py icc ratlsmooth
+python3 py/test_solved.py ratlsmooth
+
+#python3 py/permute_vectors.py min
+#python3 py/csv_multiply.py icc min
+#python3 py/test_solved.py min
+
+# extra
+
+#python3 py/csv_permute.py exotic
+#python3 py/csv_invert.py exotic
 
 #python3 py/find_duplicates.py # only does mode 'all'
 
