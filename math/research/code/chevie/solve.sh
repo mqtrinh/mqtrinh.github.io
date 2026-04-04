455da72791427965bcebfc7ff9b70dca472864cd
Minh-Tâm Quang Trinh <mqtrinh@gmail.com>
2026-04-04 14:09:45 -0400

    Add files via upload

--- math/research/code/chevie/solve.sh
+++ math/research/code/chevie/solve.sh
@@ -4,21 +4,17 @@
 #python3 py/csv_icc.py
 #python3 py/csv_invert.py icc
 
-python3 py/permute_vectors.py all
-python3 py/csv_multiply.py icc all
-python3 py/test_solved.py all
+#python3 py/permute_vectors.py all
+#python3 py/csv_multiply.py icc all
+#python3 py/test_solved.py all
 
-#python3 py/permute_vectors.py defectless
-#python3 py/csv_multiply.py icc defectless
-#python3 py/test_solved.py defectless
+#python3 py/permute_vectors.py ratlsmooth
+#python3 py/csv_multiply.py icc ratlsmooth
+#python3 py/test_solved.py ratlsmooth
 
-python3 py/permute_vectors.py ratlsmooth
-python3 py/csv_multiply.py icc ratlsmooth
-python3 py/test_solved.py ratlsmooth
-
-#python3 py/permute_vectors.py min
-#python3 py/csv_multiply.py icc min
-#python3 py/test_solved.py min
+python3 py/permute_vectors.py min
+python3 py/csv_multiply.py icc min
+python3 py/test_solved.py min
 
 # extra
 
